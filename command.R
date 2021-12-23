58d9591ff1e5e2ac275c659bab8a1aba791a0d50
Raffaele Calogero <raffaele.calogero@gmail.com>
2021-12-23 18:13:26 +0100

    Delete command.R

--- command.R
+++
@@ -1,9 +0,0 @@
-data <- read.table("annotated_GSM3618014_gene_count.csv", sep=",", header=T, row.names=1)
-clusters <- read.table("RNA-5c_clustering.output.csv", sep=",", header=T, row.names=1)
-names(clusters)
-identical(rownames(clusters), names(data))
-common <- intersect(names(data), rownames(clusters))
-data <- data[, which(names(data)%in%common)]
-identical(rownames(clusters), names(data))
-names(data) <- clusters$annotated
-write.table(data, "RNA-5c.csv", sep=",", col.names=NA)
